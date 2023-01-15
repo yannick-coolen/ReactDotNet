62c3d60207f319c64eee060dea4830de3e1986e4
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2023-01-15 22:25:39 +0100

    Updated the UX by adjusted the loading funtion

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,3 +1,4 @@
+import { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, List, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
@@ -5,13 +6,25 @@ interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityList({
   activities,
   selectActivity,
   deleteActivity,
+  submitting,
 }: Props) {
+  const [target, setTarget] = useState("");
+
+  const handleActivityDelete = (
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  };
+
   return (
     <Segment>
       <Item.Group divided>
@@ -35,7 +48,9 @@ export default function ActivityList({
                     color={"blue"}
                   />
                   <Button
-                    onClick={() => deleteActivity(activity.id)}
+                    name={activity.id}
+                    loading={submitting && target === activity.id}
+                    onClick={(e) => handleActivityDelete(e, activity.id)}
                     floated={"right"}
                     content={"Delete"}
                     color={"red"}
