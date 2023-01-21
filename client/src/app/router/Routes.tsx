80866a1c113aad2164a276ec4ac73860932a8a5d
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2023-01-21 23:46:34 +0100

    Initiate routing

---
+++ client/src/app/router/Routes.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { RouteObject } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import HomePage from "../../features/home/HomePage";
+
+const App = React.lazy(() => import("../layout/App"));
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "activities", element: <ActivityDashboard /> },
+      { path: "activities/:id", element: <ActivityDetails /> },
+      { path: "createActivity", element: <ActivityForm key={"create"} /> },
+      { path: "manage/:id", element: <ActivityForm key={"manage"} /> },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
