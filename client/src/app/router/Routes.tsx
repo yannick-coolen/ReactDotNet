842d7da9ae2ba3a105e9d36a70184e69a43f7f70
YannickAtExact <yannick-coolen-1991@hotmail.nl>
2023-01-30 00:21:21 +0100

    Updated Routes

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { RouteObject } from "react-router";
+import { Navigate, RouteObject } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
+import TestErrors from "../../features/errors/TestError";
 
 const App = React.lazy(() => import("../layout/App"));
 
@@ -16,6 +19,10 @@ export const routes: RouteObject[] = [
       { path: "activities/:id", element: <ActivityDetails /> },
       { path: "createActivity", element: <ActivityForm key={"create"} /> },
       { path: "manage/:id", element: <ActivityForm key={"manage"} /> },
+      { path: "errors", element: <TestErrors /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "server-error", element: <ServerError /> },
+      { path: "*", element: <Navigate replace to={"/not-found"} /> },
     ],
   },
 ];
